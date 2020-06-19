a027cec43709d433203cb6f4e7f3bdaaf6b4e21a
superleeyom <635709492@qq.com>
2020-06-19 14:56:16 +0800

    增加mvc配置项

---
+++ src/main/java/com/leeyom/scaffold/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.leeyom.scaffold.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * MVC配置
+ *
+ * @author leeyom
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
