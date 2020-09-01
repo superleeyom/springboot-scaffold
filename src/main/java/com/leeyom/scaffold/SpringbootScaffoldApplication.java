8828d6aa5ed9f351442513f99ad206e0cc7fbd86
superleeyom <635709492@qq.com>
2020-09-01 11:29:49 +0800

    使用阿里编码规约扫描项目

--- src/main/java/com/leeyom/scaffold/SpringbootScaffoldApplication.java
+++ src/main/java/com/leeyom/scaffold/SpringbootScaffoldApplication.java
@@ -3,6 +3,11 @@ package com.leeyom.scaffold;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author leeyom
+ */
 @SpringBootApplication
 public class SpringbootScaffoldApplication {
 
